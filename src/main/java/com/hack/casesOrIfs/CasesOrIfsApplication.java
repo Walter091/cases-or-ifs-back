ea30bd192c42a070bb6b000a2d96f623f4c5f83f
Walter091 <walterjuniorgomes091@gmail.com>
2024-11-15 19:22:51 -0300

    Commit init

---
+++ src/main/java/com/hack/casesOrIfs/CasesOrIfsApplication.java
@@ -0,0 +1,13 @@
+package com.hack.casesOrIfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasesOrIfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasesOrIfsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hack/casesOrIfs/CasesOrIfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.casesOrIfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasesOrIfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
