54f74e5f6efb5d22ad7552e39a1967452a207408
Walter091 <walterjuniorgomes091@gmail.com>
2024-11-17 17:46:28 -0300

    permitindo todos acessos

--- src/main/java/com/hack/casesOrIfs/configs/WebConfig.java
+++ src/main/java/com/hack/casesOrIfs/configs/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // URL do frontend React
+                .allowedOrigins("*") // URL do frontend React
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
